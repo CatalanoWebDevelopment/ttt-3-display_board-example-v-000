bb57c77645262e172196a8274e2986bd82de675d
Parker Catalano <catalano.work@gmail.com>
2017-09-28 17:29:56 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,9 +3,9 @@ def display_board
   puts "A Tic Tac Toe Board";
   row = "   |   |   ";
   line = "-----------";
-  print row;
-  print line;
-  print row;
-  print line;
-  print row;
+  puts row;
+  puts line;
+  puts row;
+  puts line;
+  puts row;
 end
